6ac560f7a7b35c637a3a8ef6b29a2539d6705130
Abdullah <mahdyabdullah6@gmail.com>
2021-02-04 20:12:00 +0300

    Add Class Person

---
+++ Bank Account/Person.java
@@ -0,0 +1,58 @@
+package bank.account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private String ID;
+    private String Email;
+
+    private List<Account> accounts;
+
+    public Person(String name, String ID, String Email) {
+        this.name = name;
+        this.ID = ID;
+        this.Email = Email;
+        this.accounts = new ArrayList();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", ID=" + ID + ", Email=" + Email + ", accounts=" + accounts + '}';
+    }
+
+}
